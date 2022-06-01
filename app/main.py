7772af551f075cf79606c3ba989b94684d5ac661
aomntw <nuntawat.thsrj@gmail.com>
2022-06-01 11:59:17 +0700

    test

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ app = FastAPI(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Worldggg"}
+    return {"Hello": "Worldkkk"}
 
 
 @app.post("/conversation/unit",tags=["Conversation Unit"])
